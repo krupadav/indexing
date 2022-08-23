fdfb9ad97e1fb4316e7f5981d982d687fbb67c49
krupadav <107058808+krupadav@users.noreply.github.com>
2022-08-23 23:07:08 +0530

    Add files via upload

---
+++ indexing.py
@@ -0,0 +1,7 @@
+a = [1, 2, 25, 29]
+print(a[0])
+print(a[1:3])
+print(a[::-1]) #reverse the sequence
+b = [1, 2, 3, 4, 5, 6, 7, 8]
+print(a[::2])  # all even position
+print(a[1::2]) # all odd position
